b097b43f84937410ba774b2548f4cfa23ac50750
Kevin Grant <kevingrant5@gmail.com>
2023-08-23 00:07:08 -0400

    Use the shared env on the resourceList page

--- html/resourceList.py
+++ html/resourceList.py
@@ -21,86 +21,53 @@
 """
 
 import os
-import sys
-import cgi
-from http import cookies
-import dbSession
 import dbShared
-import pymysql
 import ghShared
 import env
 import ghLists
 from jinja2 import Environment, FileSystemLoader
+from utils import setup_user_environment
 
-# Get current url
-try:
-	url = os.environ['SCRIPT_NAME']
-except KeyError:
-	url = ''
-uiTheme = ''
-form = cgi.FieldStorage()
-# Get Cookies
-useCookies = 1
-C = cookies.SimpleCookie()
-try:
-	C.load(os.environ['HTTP_COOKIE'])
-except KeyError:
-	useCookies = 0
+def main():
+	user_env = setup_user_environment()
+	url=user_env['url']
+	currentUser=user_env['currentUser']
+	loginResult=user_env['loginResult']
+	uiTheme=user_env['uiTheme']
+	galaxy=user_env['galaxy']
+	logged_state=user_env['logged_state']
+	linkappend=user_env['linkappend']
+	pictureName=user_env['pictureName']
 
-if useCookies:
-	try:
-		currentUser = C['userID'].value
-	except KeyError:
-		currentUser = ''
-	try:
-		loginResult = C['loginAttempt'].value
-	except KeyError:
-		loginResult = 'success'
-	try:
-		sid = C['gh_sid'].value
-	except KeyError:
-		sid = form.getfirst('gh_sid', '')
-	try:
-		uiTheme = C['uiTheme'].value
-	except KeyError:
-		uiTheme = ''
-	try:
-		galaxy = C['galaxy'].value
-	except KeyError:
-		galaxy = form.getfirst('galaxy', ghShared.DEFAULT_GALAXY)
-else:
-	currentUser = ''
-	loginResult = form.getfirst('loginAttempt', '')
-	sid = form.getfirst('gh_sid', '')
-	galaxy = form.getfirst('galaxy', ghShared.DEFAULT_GALAXY)
+	resCategories = ['Creature Food','Creature Structural','Flora Food','Flora Structural','Chemical','Water','Mineral','Gas','Energy']
+	resCategoryIDs = ['creature_food','creature_structural','flora_food','flora_structural','chemical','water','mineral','gas','energy_renewable']
 
-# escape input to prevent sql injection
-sid = dbShared.dbInsertSafe(sid)
+	pictureName = dbShared.getUserAttr(currentUser, 'pictureName')
+	print('Content-type: text/html\n')
+	environ = Environment(loader=FileSystemLoader('templates'))
+	environ.globals['BASE_SCRIPT_URL'] = ghShared.BASE_SCRIPT_URL
+	environ.globals['MOBILE_PLATFORM'] = ghShared.getMobilePlatform(os.environ['HTTP_USER_AGENT'])
+	template = environ.get_template('resourcelist.html')
 
-# Get a session
-logged_state = 0
-linkappend = ''
-if loginResult == None:
-	loginResult = 'success'
+	print(template.render(
+		uiTheme=uiTheme, 
+		loggedin=logged_state, 
+		currentUser=currentUser, 
+		loginResult=loginResult, 
+		linkappend=linkappend, 
+		url=url, 
+		pictureName=pictureName, 
+		imgNum=ghShared.imgNum, 
+		galaxyList=ghLists.getGalaxyList(), 
+		resourceTypeList=ghLists.getResourceTypeList(galaxy), 
+		resourceGroupList=ghLists.getResourceGroupList(), 
+		planetList=ghLists.getPlanetList(galaxy), 
+		resCategories=resCategories, 
+		resCategoryIDs=resCategoryIDs, 
+		enableCAPTCHA=env.RECAPTCHA_ENABLED, 
+		siteidCAPTCHA=env.RECAPTCHA_SITEID
+		)
+	)
 
-sess = dbSession.getSession(sid)
-if (sess != ''):
-	logged_state = 1
-	currentUser = sess
-	if (uiTheme == ''):
-		uiTheme = dbShared.getUserAttr(currentUser, 'themeName')
-	if (useCookies == 0):
-		linkappend = 'gh_sid=' + sid
-else:
-	if (uiTheme == ''):
-		uiTheme = 'crafter'
-resCategories = ['Creature Food','Creature Structural','Flora Food','Flora Structural','Chemical','Water','Mineral','Gas','Energy']
-resCategoryIDs = ['creature_food','creature_structural','flora_food','flora_structural','chemical','water','mineral','gas','energy_renewable']
-
-pictureName = dbShared.getUserAttr(currentUser, 'pictureName')
-print('Content-type: text/html\n')
-environ = Environment(loader=FileSystemLoader('templates'))
-environ.globals['BASE_SCRIPT_URL'] = ghShared.BASE_SCRIPT_URL
-environ.globals['MOBILE_PLATFORM'] = ghShared.getMobilePlatform(os.environ['HTTP_USER_AGENT'])
-template = environ.get_template('resourcelist.html')
-print(template.render(uiTheme=uiTheme, loggedin=logged_state, currentUser=currentUser, loginResult=loginResult, linkappend=linkappend, url=url, pictureName=pictureName, imgNum=ghShared.imgNum, galaxyList=ghLists.getGalaxyList(), resourceTypeList=ghLists.getResourceTypeList(galaxy), resourceGroupList=ghLists.getResourceGroupList(), planetList=ghLists.getPlanetList(galaxy), resCategories=resCategories, resCategoryIDs=resCategoryIDs, enableCAPTCHA=env.RECAPTCHA_ENABLED, siteidCAPTCHA=env.RECAPTCHA_SITEID))
+if __name__ == "__main__":
+	main()
